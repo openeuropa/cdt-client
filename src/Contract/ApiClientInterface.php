a6597d44e4d19114ba119b59a3d89fdee7d4d196
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-03-07 08:08:27 +0100

    D8AGE-378: The endpoint test for files API. Changed FileList into array.

--- src/Contract/ApiClientInterface.php
+++ src/Contract/ApiClientInterface.php
@@ -6,7 +6,7 @@ namespace OpenEuropa\CdtClient\Contract;
 
 use OpenEuropa\CdtClient\Exception\ValidationErrorsException;
 use OpenEuropa\CdtClient\Model\Request\Translation as TranslationRequest;
-use OpenEuropa\CdtClient\Model\Response\FileList;
+use OpenEuropa\CdtClient\Model\Response\File;
 use OpenEuropa\CdtClient\Model\Response\Translation as TranslationResponse;
 use OpenEuropa\CdtClient\Model\Response\ReferenceData;
 use OpenEuropa\CdtClient\Model\Token;
@@ -48,8 +48,10 @@ interface ApiClientInterface
     public function getRequestStatus(string $permanentId): TranslationResponse;
 
     /**
+     * @return array<int, File>
+     *
      * @throws ValidationErrorsException
      * *   Thrown if there are validation errors.
      */
-    public function getTranslatedFiles(string $permanentId): FileList;
+    public function getTranslatedFiles(string $permanentId): array;
 }
