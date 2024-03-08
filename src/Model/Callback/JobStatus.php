4f21d95d9ef48475119434dcabd5dc735ebe49d0
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-03-08 15:47:37 +0100

    D8AGE-285: Callback DTOs.

---
+++ src/Model/Callback/JobStatus.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEuropa\CdtClient\Model\Callback;
+
+/**
+ * Class JobStatus.
+ *
+ * Represents the single callback sent to the defined endpoint
+ * @todo All properties are Uppercase in the request.
+ */
+class JobStatus
+{
+    protected string $requestIdentifier;
+
+    protected string $status;
+
+    protected string $sourceDocumentName;
+
+    protected string $sourceLanguageCode;
+
+    protected string $targetLanguageCode;
+
+    public function getRequestIdentifier(): string
+    {
+        return $this->requestIdentifier;
+    }
+
+    public function setRequestIdentifier(string $requestIdentifier): self
+    {
+        $this->requestIdentifier = $requestIdentifier;
+        return $this;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function getSourceDocumentName(): string
+    {
+        return $this->sourceDocumentName;
+    }
+
+    public function setSourceDocumentName(string $sourceDocumentName): self
+    {
+        $this->sourceDocumentName = $sourceDocumentName;
+        return $this;
+    }
+
+    public function getSourceLanguageCode(): string
+    {
+        return $this->sourceLanguageCode;
+    }
+
+    public function setSourceLanguageCode(string $sourceLanguageCode): self
+    {
+        $this->sourceLanguageCode = $sourceLanguageCode;
+        return $this;
+    }
+
+    public function getTargetLanguageCode(): string
+    {
+        return $this->targetLanguageCode;
+    }
+
+    public function setTargetLanguageCode(string $targetLanguageCode): self
+    {
+        $this->targetLanguageCode = $targetLanguageCode;
+        return $this;
+    }
+}
---
+++ src/Model/Callback/RequestStatus.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEuropa\CdtClient\Model\Callback;
+
+/**
+ * Class RequestStatus.
+ *
+ * Represents the single callback sent to the defined endpoint
+ * @todo all properties are Uppercase in the request.
+ */
+class RequestStatus
+{
+    protected string $requestIdentifier;
+
+    protected string $status;
+
+    protected \DateTimeInterface $date;
+
+    protected string $correlationId;
+
+    public function getRequestIdentifier(): string
+    {
+        return $this->requestIdentifier;
+    }
+
+    public function setRequestIdentifier(string $requestIdentifier): self
+    {
+        $this->requestIdentifier = $requestIdentifier;
+        return $this;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function getDate(): \DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    public function getCorrelationId(): string
+    {
+        return $this->correlationId;
+    }
+
+    public function setCorrelationId(string $correlationId): self
+    {
+        $this->correlationId = $correlationId;
+        return $this;
+    }
+}
