510cd46a94ca645f67315c6d4586d01ce842db8e
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-05-06 13:40:06 +0200

    D8AGE-382: Add file downloader.

---
+++ src/Http/Download.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEuropa\CdtClient\Http;
+
+use OpenEuropa\CdtClient\Contract\RestInterface;
+use OpenEuropa\CdtClient\Contract\TokenAwareInterface;
+use OpenEuropa\CdtClient\Exception\InvalidStatusCodeException;
+use OpenEuropa\CdtClient\Traits\SerializerAwareTrait;
+use OpenEuropa\CdtClient\Traits\TokenAwareTrait;
+use OpenEuropa\CdtClient\Traits\ValidationAwareTrait;
+
+/**
+* A class that download files from the API.
+ *
+ * @see TokenAwareInterface
+ * @see ValidationAwareTrait
+ * @see
+*/
+class Download implements TokenAwareInterface
+{
+    use TokenAwareTrait;
+    use ValidationAwareTrait;
+    use SerializerAwareTrait;
+
+    public function __construct(protected RestInterface $rest)
+    {
+    }
+
+    /**
+     * @param array<string, string> $headers
+     */
+    public function downloadFile(string $uri, array $headers = []): string
+    {
+        try {
+            $response = $this->rest->get($uri, $this->getAuthorizationHeaders());
+            return $response->getBody()->__toString();
+        } catch (InvalidStatusCodeException $e) {
+            throw $this->dispatchValidationException($e);
+        }
+    }
+}
