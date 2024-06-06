39590340043f5fb551910e5a000f0cdf9b527128
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-06-06 12:12:03 +0200

    D8AGE-487: Remove obsolete use clauses.

--- src/Contract/RestInterface.php
+++ src/Contract/RestInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace OpenEuropa\CdtClient\Contract;
 
-use OpenEuropa\CdtClient\Model\Response\Token;
 use Psr\Http\Message\ResponseInterface;
 
 interface RestInterface
